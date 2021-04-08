ed5fe97234e841263151ec7fdad8a43bc7f996e1
TristanDiabolo <tristan@diabolo-web.com>
2021-04-08 14:48:13 +0200

    better sticky navbar

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -1,8 +1,23 @@
 import Sticky from 'sticky-js';
 
+const nav = document.querySelector('nav');
+// For event infos
 let sticky = new Sticky('.sticky-top-element', {
   stickyWrap: true,
-  marginTop: document.querySelector('nav').offsetHeight + 30,
+  marginTop: 70,
   stickyFor: 992,
   stickyClass: 'sticked',
+});
+
+// For header sticky top
+const changeNavWhenSticked = () => {
+  if (window.scrollY > 50) {
+    nav.classList.add('sticked');
+  } else {
+    nav.classList.remove('sticked');
+  }
+};
+changeNavWhenSticked();
+window.addEventListener('scroll', () => {
+  changeNavWhenSticked();
 });
